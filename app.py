37476a2111d12b5b2bd1f4ee77764af1385abe4a
chloekim917 <chloekim917@gmail.com>
2023-04-20 16:18:33 +0900

    timer input values restricted

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-hr, min, sec = input("Set your countdown clock. Write hour, minutes, and seconds separted by :. If you don't want to set anything, write 0 for that field: ").split(":")
-hrInt = int(hr)
-minInt = int(min)
-secInt = int(sec)
-userInput = '{:02}:{:02}:{:02}'.format(hrInt, minInt, secInt)
-print(f"The timer is set for: {hr} hours {min} minutes and {sec} seconds")
-t = (hrInt*3600)+(minInt*60)+secInt
+while True:
+    hr, min, sec = input("Set your countdown clock. Write hour, minutes, and seconds separted by :. If you don't want to set anything, write 0 for that field: ").split(":")
+    hrInt = int(hr)
+    minInt = int(min)
+    secInt = int(sec)
+    
+    if hrInt > 23:
+        print("Sorry, I can't count longer than one day. Let's try this one more time.")
+    elif minInt > 60:
+        print("There are only 60 mins in an hour. Please enter some value smaller than 60 for minutes.")
+    elif secInt > 60:
+        print("There are only 60 seconds in a minute. Please enter some value smaller than 60 for seconds.")
+    else:
+        userInput = '{:02}:{:02}:{:02}'.format(hrInt, minInt, secInt)
+        print(f"The timer is set for: {hr} hours {min} minutes and {sec} seconds")
+        break
 
+t = (hrInt*3600)+(minInt*60)+secInt
 
 import time
 def countdown(t):
@@ -14,7 +24,7 @@ def countdown(t):
         minInt, secInt = divmod(minInt, 60)
         timer = '{:02d}:{:02d}:{:02d}'.format(hrInt, minInt, secInt)
         print(timer, end="\r")
-        time.sleep(1)
+        time.sleep(0.001)
         t -= 1
     print("Bzzt! Bzzt! Time's up!")
 
