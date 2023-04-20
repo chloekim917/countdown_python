8d2bfd74b53f92032dc0eba047b467735fc585e4
chloekim917 <chloekim917@gmail.com>
2023-04-20 15:31:01 +0900

    minutes and secs countdown done

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 hr, min, sec = input("Set your countdown clock. Write hour, minutes, and seconds separted by :. If you don't want to set anything, write 0 for that field: ").split(":")
-userInput = '{:02}:{:02}:{:02}'.format(int(hr), int(min), int(sec))
+hrInt = int(hr)
+minInt = int(min)
+secInt = int(sec)
+userInput = '{:02}:{:02}:{:02}'.format(hrInt, minInt, secInt)
 print(userInput)
+t = (minInt*60)+secInt
+print (t)
+
+import time
+def countdown(t):
+    while t: 
+        minInt, secInt = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(minInt, secInt)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print("Bzzt! Bzzt! Time's up!")
+
+countdown(t)
 
 
 """
